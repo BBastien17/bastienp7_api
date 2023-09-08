f6975ca577b12656427052b574298f84636dce02
BBastien17 <106978157+BBastien17@users.noreply.github.com>
2023-09-09 00:05:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -133,8 +133,8 @@ def add():
         print("Voici la variable todos[index] : ", todos[index])
         print("Voici la variable todos dans la fonction add : ", todos)
         new_todos = todos
-        return redirect(url_for('client_description'), new_todos=new_todos)
-    return render_template('add.html')
+        return (redirect(url_for('client_description')), new_todos)
+    return render_template('add.html', new_todos=new_todos)
 
 @app.route('/client_description', methods = ['GET', 'POST'])
 def client_description():

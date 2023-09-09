a2e25c8d32ef55b14a5c6a2362202da9fd98c11f
BBastien17 <106978157+BBastien17@users.noreply.github.com>
2023-09-09 09:32:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -137,12 +137,13 @@ def add():
         #new_todos = todos
         return redirect(url_for('client_description', name = id_get_client))
     id_get_client = request.form.get("id_client")
-    return (render_template('add.html'), id_get_client)#, save_todos = save_todos)
+    return (render_template('add.html'), name = id_get_client)#, save_todos = save_todos)
 
 @app.route('/client_description', methods = ['GET', 'POST'])
 def client_description():
-    detail_add, id_get_client = add()
+    detail_add, name = add()
     print("id_get_client est : ", id_get_client)
+    print("detail_add est : ", detail_add)
     todos = {}
     todos.clear()
     print("taille de la variable todos avant operation : ", len(todos))

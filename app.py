226b87264e54bdb7dfbdf71a69894d151e6e99ad
BBastien17 <106978157+BBastien17@users.noreply.github.com>
2023-09-12 00:26:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -137,9 +137,8 @@ def add():
 
 
 @app.route('/client_description', methods = ['GET', 'POST'])
-def client_description():
+def client_description(todos):
     
-    #detail_add, name = add()
     print("id_get_client est : ", todos)
     #print("detail_add est : ", detail_add)
     #todos = {}

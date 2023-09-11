969e8e192bb2e714ded89ae75a8821113d8c41d7
BBastien17 <106978157+BBastien17@users.noreply.github.com>
2023-09-11 22:33:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -130,6 +130,7 @@ def add():
         print("Voici la variable todos[index] : ", todos[index])
         print("Voici la variable todos dans la fonction add : ", todos)    
         return redirect(url_for('client_description'))
+        return render_template('client_description'.html)
     #return redirect(url_for('add'))#, save_todos = todos))
     return render_template('add.html')
 

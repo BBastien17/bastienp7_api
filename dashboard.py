4d256d4dea540104680cb1806862ccf1b4599498
BBastien17 <106978157+BBastien17@users.noreply.github.com>
2023-09-15 21:38:54 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,25 +3,11 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib import figure
 import requests
-import math
-import seaborn as sns
-from sklearn.preprocessing import LabelEncoder
-import joblib
-#from sklearn.metrics import classification_report, confusion_matrix
-import mlflow.sklearn
-#Librairie pour XGBoostClassifier
-from xgboost import XGBClassifier
-#import lime
-#from lime import lime_tabular
-from sklearn.model_selection import train_test_split
-import pickle
-#import pickle5 as pickle
 
 #Importation des fonctions contenants les pages
-from page_prospect import page_p
-from page_client import page_c
+#from page_prospect import page_p
+#from page_client import page_c
 
 print("Ouverture de la page dashboard.py")
 
@@ -44,7 +30,7 @@ def request_prediction(model_uri, data):
 page = st.sidebar.selectbox('Page Navigation', ["Prospects", "Clients"])
 st.sidebar.markdown("""---""")
 st.sidebar.write("Created by Bastien B")
-st.sidebar.image("images/logo2.png", width=100)
+st.sidebar.image("./images/logo2.png", width=100)
 
 path = 'Projet_7/'
 
@@ -53,10 +39,6 @@ data_work = pd.read_csv("./data_work.csv")
 data_target = pd.read_csv("./data_target.csv")
 data_complete = data_work.copy()
 data_complete["Target"] = data_target
-
-#Import du modèle XGBClassifier
-with open(f'xgb_model_final/model.pkl', 'rb') as f:
-    model = pickle.load(f)
 
 #Appel de la page correspondante en fonction du choix réalisé dans le menu déroulant
 #if page == "Prospects": 

87e0161919cf8cedf22dc0aca9ee0c2607c21904
BBastien17 <106978157+BBastien17@users.noreply.github.com>
2023-10-04 17:23:04 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
-mkdir -p ~/.streamlit
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
-" > ~/.streamlit/config.tomlbashCopy code
-streamlit run dashboard.py
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"boyer_bastien@msn.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml

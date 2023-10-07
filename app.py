280e8c722390f764941417131732aa5bed7b6bf0
BBastien17 <106978157+BBastien17@users.noreply.github.com>
2023-10-07 23:13:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -53,63 +53,7 @@ def calc_score_predictproba (ref_client, data_work_complet):
                         'Montant_anticipation_pret':data_work_client['Montant_anticipation_pret'],
                         'Delai_anticipation_pret':data_work_client['Delai_anticipation_pret']}
 
-#Fonction pour calculer le score prédictproba du client
-@app.post("/calc_score_predictproba_streamlit")
-def calc_score_predictproba_streamlit (ref_client, data_work_complet):
-    print("Lancement de la fonction calc_score_predictproba")
-    #Création d'un dataframe avec les information du client sélectionné :
-    data_work_client = pd.DataFrame(data_work_complet,index=[ref_client])
-    print(data_work_client)
-    #Création du dictionnaire où l'on stocke les résultats
-    list_result_work = {'Type_de_pret':data_work_client['Type_de_pret'], 'Genre':data_work_client['Genre'],
-                        'Age':data_work_client['Age'], 'Niveau_d_etudes':data_work_client['Niveau_d_etudes'],
-                        'Regime_matrimonial':data_work_client['Regime_matrimonial'],
-                        'Nb_enfants': data_work_client['Nb_enfants'],
-                        'Nb_membre_famille':data_work_client['Nb_membre_famille'], 
-                        'Montant_des_revenus':data_work_client['Montant_des_revenus'],
-                        'Note_region_client':data_work_client['Note_region_client'],
-                        'Nb_demande_client':data_work_client['Nb_demande_client'],
-                        'Montants_du_pret':data_work_client['Montants_du_pret'],
-                        'Montant_des_annuites':data_work_client['Montant_des_annuites'],
-                        'Nb_jours_credits':data_work_client['Nb_jours_credits'],                       
-                        'Montant_anticipation_pret':data_work_client['Montant_anticipation_pret'],
-                        'Delai_anticipation_pret':data_work_client['Delai_anticipation_pret']}
-    
-    #On transforme ensuite le dictionnaire en dataframe
-    data_work_list_result = pd.DataFrame(data=list_result_work)
-    print("Conversion du dictionnaire en dataframe des caractéristiques client : ", data_work_list_result)
-    #On oublie pas de préparer la transformation des variables catégorielles en variables numériques plus tard
-    transf_data_work_categ = {'Prêts de trésorerie': 0, 'Prêts renouvelables': 1,
-                              'autre': 0, 'célibataire': 1, 'marié(e)': 2,
-                              'M': 1, 'F': 0,
-                              '3 à 4': 0, '5 à 8': 1}    
-    print("conversion des variables en variables numériques : ", transf_data_work_categ)
-    #Découpage des datasets en dataset de train et de test (proportion 80/20)
-    X_train, X_test, y_train, y_test = train_test_split(data_work_complet, data_target_complet, test_size = 0.2, random_state=42)
-    #Librairie pour encoder des variables catégorielles
-    encoder = LabelEncoder()
-    #On remet la variable concernant une période sous un format positif
-    X_test['Delai_anticipation_pret'] = X_test['Delai_anticipation_pret'].mul(-1)
-    #On remet la variable concernant une période sous un format positif
-    X_train['Delai_anticipation_pret'] = X_train['Delai_anticipation_pret'].mul(-1)
-    #Création d'une variable avec la liste des colonnes catégorielles du dataset features
-    data_categ = list(data_work_complet.select_dtypes(exclude=["number"]).columns)
-    print("affichage de data_categ : ", data_categ)
-    print("X_train : ", X_train.head())
-    print("X_test : ", X_test.head())
-    #Encodage des variables catégorielles
-    for col in data_categ:
-        X_train[col] = encoder.fit_transform(X_train[col])
-        X_test[col] = encoder.fit_transform(X_test[col])
-    print("X_train_transform : ", X_train.head())
-    print("X_test_transform : ", X_test.head())
-    #On transforme les variables catégorielles en variables numériques
-    data_work_list_result_transf = data_work_list_result.replace(transf_data_work_categ)
-    print("Voici le dataset après transformation des variables catégorielles en numériques : ", data_work_list_result_transf)
-    #Prédiction du résultat
-    score = model.predict_proba(data_work_list_result_transf)
-    print("affichage du score : ", score)
-    return score
+
 
 
 def is_sourcefile(path):

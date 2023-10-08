10f00f6f7ea02b572da842c782a52ca0ff672670
BBastien17 <106978157+BBastien17@users.noreply.github.com>
2023-10-09 00:41:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -35,8 +35,8 @@ print(data_work_complet.head())
 data_target_complet = pd.read_csv("./data_target.csv")
 print(data_target_complet.head())
 
-githubAPIURL2 = "https://api.github.com/repos/BBastien17/bastienp7_api/contents/pred.csv"
-githubAPIURL3 = "https://api.github.com/repos/BBastien17/bastienp7_api/contents/score.csv"
+githubAPIURL2 = "https://api.github.com/repos/BBastien17/bastienp7_api/contents/pred"
+githubAPIURL3 = "https://api.github.com/repos/BBastien17/bastienp7_api/contents/score"
 githubToken = "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"
 
 #Fonction pour calculer le score prédictproba du client
@@ -168,7 +168,7 @@ def data_stream():
     pred = model.predict(conv_data_csv)
     #pred = str(pred)
     print("Affichage de la variable target : ", pred)
-    with open("pred.csv", "rb") as f:
+    with open("pred", "rb") as f:
         # Encoding "my-local-image.jpg" to base64 format
         encodedData = base64.b64encode(f.read())
 
@@ -188,7 +188,7 @@ def data_stream():
     score = model.predict_proba(conv_data_csv)
     #score = str(score)
     print("Affichage du score predictproba : ", score)
-    with open("score.csv", "rb") as f:
+    with open("score", "rb") as f:
       # Encoding "my-local-image.jpg" to base64 format
       encodedData = base64.b64encode(f.read())
 
